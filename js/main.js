a4e6fa9e18f221677d5b7046b903f56c1a48c9ec
Aditi Ohri <didiohri@gmail.com>
2019-11-01 17:15:20 -0500

    update readme to do tech

---
+++ js/main.js
@@ -0,0 +1,8 @@
+/*----- constants -----*/
+const glyphAnswer = [];
+/*----- app's state (variables) -----*/
+let playerScore = [];
+
+/*----- cached element references -----*/
+/*----- event listeners -----*/
+/*----- functions -----*/
