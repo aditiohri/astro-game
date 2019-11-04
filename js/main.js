def8870c24eaebc749524be19bfb3e503767b6f2
Aditi Ohri <didiohri@gmail.com>
2019-11-03 18:48:03 -0600

    update js quiz structure

--- js/main.js
+++ js/main.js
@@ -1,8 +1,165 @@
 /*----- constants -----*/
-const glyphAnswer = [];
+
+const astroQuestions = [
+    {
+      question: `This glyph, symbolic of a ram, represents the first sign of the zodiac. This fire sign is bold, action-oriented and assertive. Which sign is it?`,
+      answers: {
+        a: "Taurus",
+        b: "Cancer",
+        c: "Aries",
+        d: "Capricorn",
+      },
+      correctAnswer: "c"
+    },
+    {
+      question: `The earth sign represented by this glyph is one of two ruled by Venus. Associated with fertility, abundance and luxury, this glyph is symbolic of a bull. Which sign is it?`,
+      answers: {
+        a: "Capricorn",
+        b: "Libra",
+        c: "Pisces",
+        d: "Taurus",
+      },
+      correctAnswer: "d"
+    },
+    {
+      question: `This sign loves to chat. Highly intellectual, witty and adaptable, the glyph for this air sign is a symbol for twins, speaking to its dualistic nature. Which sign is it?`,
+      answers: {
+        a: "Gemini",
+        b: "Scorpio",
+        c: "Aries",
+        d: "Leo",
+      },
+      correctAnswer: "a"
+    },
+    {
+      question: `Some say this water sign has a hard shell. Known for being emotional intuitive, and deeply maternal, some say its glyph represent breasts, the primary source of nourishment for babies. Which sign is it?`,
+      answers: {
+        a: "Scorpio",
+        b: "Cancer",
+        c: "Aquarius",
+        d: "Taurus",
+      },
+      correctAnswer: "b"
+    },
+    {
+      question: `Fixed fire sign; drama; warm; effusive; center of attention; courageous. Which sign is it?`,
+      answers: {
+        a: "Aries",
+        b: "Leo",
+        c: "Scorpio",
+        d: "Pisces",
+      },
+      correctAnswer: "b"
+    },
+    {
+      question: `Mutable earth sign; reliable; goal-oriented; great workers; attention to detail; perfectionists; caring for the body. Which sign is it?`,
+      answers: {
+        a: "Libra",
+        b: "Sagittarius",
+        c: "Cancer",
+        d: "Virgo",
+      },
+      correctAnswer: "d"
+    },
+    {
+      question: `Cardinal air sign; diplomatic; social; gracious; loves beauty; great fashion sense; Which sign is it?`,
+      answers: {
+        a: "Libra",
+        b: "Virgo",
+        c: "Aries",
+        d: "Taurus",
+      },
+      correctAnswer: "a"
+    },
+    {
+      question: `Fixed water; Dramatic, passionate probing, lover of mysteries + understanding human psychology, secretive, tenacious; Which sign is it?`,
+      answers: {
+        a: "Sagittarius",
+        b: "Capricorn",
+        c: "Scorpio",
+        d: "Pisces",
+      },
+      correctAnswer: "b"
+    },
+    {
+      question: `Mutable fire; Philosophical; adventurous; independent; restless; optimistic; Which sign is it?`,
+      answers: {
+        a: "Aries",
+        b: "Leo",
+        c: "Sagittarius",
+        d: "Cancer",
+      },
+      correctAnswer: "c"
+    },
+    {
+      question: `Cardinal earth. Industrious; resilient; authoritative; dependable; does well in structure; disciplined; Which sign is it?`,
+      answers: {
+        a: "Capricorn",
+        b: "Leo",
+        c: "Virgo",
+        d: "Aquarius",
+      },
+      correctAnswer: "a"
+    },
+    {
+      question: `Fixed air; humanitarian; eccentric; idealistic; detached; inventive; opinionated; Which sign is it?`,
+      answers: {
+        a: "Leo",
+        b: "Libra",
+        c: "Gemini",
+        d: "Aquarius",
+      },
+      correctAnswer: "d"
+    },
+    {
+      question: `Mutable water; artistic; sensitive; impressionable; compassionate; tender; dreamy; Which sign is it?`,
+      answers: {
+        a: "Cancer",
+        b: "Taurus",
+        c: "Pisces",
+        d: "Leo",
+      },
+      correctAnswer: "c"
+    },
+  ]
+
 /*----- app's state (variables) -----*/
-let playerScore = [];
 
 /*----- cached element references -----*/
+  
+  const quizContainer = document.getElementById('quiz');
+  const resultsContainer = document.getElementById('results');
+  const submitButton = document.getElementById('submit');
+  
 /*----- event listeners -----*/
+
+  submitButton.addEventListener('click', showResults)
+  
+
 /*----- functions -----*/
+
+  function buildQuiz() {
+  const output = [];
+  astroQuestions.forEach((currentQ, qNumber) => {
+    const answers = [],
+    for (letter in currentQ.answers) {
+      answers.push(
+        `<label>
+        <input type="radio" name="question${qNumber}" value="${letter}">
+        ${letter}: ${currentQ.answers[letter]}
+        </label>`
+      );
+    }
+    output.push(
+      `<div class ="question"> ${currentQ.question}</div>
+      <div class="answers"${answers.join('')}</div>`
+    );
+  }
+  );
+  quizContainer.innerHTML = output.join('');
+  }
+  
+  function showResults() {
+  
+  }
+  
